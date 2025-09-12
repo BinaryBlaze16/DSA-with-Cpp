cee394dc3b541ec252fe56ab5a5e2ff67a95002c
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-12 14:24:21 +0530

    first commit

---
+++ assignment-7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of A");
+    scanf("%d",&a);
+    printf("Enter the value of B");
+    scanf("%d",&b);
+    printf("sum of A nag B is %d",a+b);
+    printf("\n");
+    return 0;
+}
